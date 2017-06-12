a169b5c6769fc9b6e6596d349f251bb077dfe07f
Mustafa Ngure <mustafangure@gmail.com>
2017-06-12 21:58:33 +0300

    loop upto squareroot of i

--- Day_one/find_primes.py
+++ Day_one/find_primes.py
@@ -10,7 +10,8 @@ def find_prime_numbers(n):
             return solution
         else:
             for i in range(2,n+1):
-                for j in range(2,i):
+                sqrt =int(i**0.5) 
+                for j in range(2,sqrt+1):
                     if i%j == 0:
                         break
                 else:
